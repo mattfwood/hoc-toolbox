c18366c2150a3c1c86d8e562b282f8d142c482d6
Matt Wood <m.wood0904@gmail.com>
2018-09-20 13:40:16 -0500

    Remove .docz

--- .docz/app/imports.js
+++
@@ -1,8 +0,0 @@
-export const imports = {
-  'src/components/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-index" */ 'src/components/index.mdx'),
-  'src/components/Filter/Filter.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-filter-filter" */ 'src/components/Filter/Filter.mdx'),
-  'src/components/If/If.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-if-if" */ 'src/components/If/If.mdx'),
-}
