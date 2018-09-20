3a80d9ec2b2bb945fc826a57e0c7871187da7b64
Matt Wood <m.wood0904@gmail.com>
2018-09-20 11:17:35 -0500

    Update docz

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,4 +1,6 @@
 export const imports = {
+  'src/components/index.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-index" */ 'src/components/index.mdx'),
   'src/components/Filter/Filter.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-components-filter-filter" */ 'src/components/Filter/Filter.mdx'),
   'src/components/If/If.mdx': () =>
