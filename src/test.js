0c06d23b15e8f0affc9eee1fe1adeef69c6d2ed9
Matt Wood <m.wood0904@gmail.com>
2018-09-19 19:51:28 -0500

    add tests

--- src/test.js
+++ src/test.js
@@ -1,7 +1,40 @@
-import ExampleComponent from './'
+import React from 'react';
+import { render } from 'react-testing-library';
+import Filter from './components/Filter';
+// import sample from './components/sample';
+const data = ['One', 'Two', 'Three'];
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
-  })
-})
+describe('Filter', () => {
+  it('filters an array of strings', () => {
+    const { getByTestId, rerender } = render(
+      <Filter data={data} search="One">
+        {results => (
+          <div data-testid="results">
+            {results.map((result, i) => (
+              <div key={i}>{result}</div>
+            ))}
+          </div>
+        )}
+      </Filter>
+    );
+
+    const results = getByTestId('results');
+    expect(results.textContent.includes('One')).toBe(true);
+
+    // Results should update on change
+    rerender(
+      <Filter data={data} search="Two">
+        {results => (
+          <div data-testid="results">
+            {results.map((result, i) => (
+              <div key={i}>{result}</div>
+            ))}
+          </div>
+        )}
+      </Filter>
+    );
+
+    const updatedResults = getByTestId('results');
+    expect(updatedResults.textContent.includes('Two')).toBe(true);
+  });
+});
